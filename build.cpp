17aa91c8d23f2aaef236ee0501bad24c5dcd9291
Mark Underwood <mtunderwood@alaska.edu>
2019-09-18 17:36:21 -0800

    Ignore .idea, add build.cpp and .h

---
+++ build.cpp
@@ -0,0 +1,4 @@
+// build.cpp
+// Created by Mark on 9/18/2019.
+//
+
---
+++ build.h
@@ -0,0 +1,8 @@
+// build.h
+// Created by Mark on 9/18/2019.
+//
+
+#ifndef BRIDGES_BUILD_H
+#define BRIDGES_BUILD_H
+
+#endif //BRIDGES_BUILD_H

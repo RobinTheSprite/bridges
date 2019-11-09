cec7a5cc9a771c5df3a0f7587a6325bdb20fe921
Mark Underwood <mtunderwood@alaska.edu>
2019-11-08 23:56:25 -0900

    Pass subset by reference

--- build.cpp
+++ build.cpp
@@ -18,7 +18,7 @@ bool containsCrossedBridges(const vector<Bridge> &subset)
     return subset[subset.size() - 2][0] >= subset.back()[0] || subset[subset.size() - 2][1] >= subset.back()[1];
 }
 
-int buildRecurse(const vector<Bridge> &bridges, vector<Bridge> subset, int startIndex)
+int buildRecurse(const vector<Bridge> &bridges, vector<Bridge> &subset, int startIndex)
 {
     auto maxToll = 0;
     auto save = subset;
@@ -42,6 +42,7 @@ int buildRecurse(const vector<Bridge> &bridges, vector<Bridge> subset, int start
 int build(int westCities, int eastCities, const vector<Bridge> &bridges)
 {
     vector<Bridge> copy = bridges;
+    vector<Bridge> emptySubset{};
     std::sort(copy.begin(), copy.end(), [](auto a, auto b) { return a[0] < b[0]; });
-    return buildRecurse(copy, vector<Bridge>{}, 0);
+    return buildRecurse(copy, emptySubset, 0);
 }
